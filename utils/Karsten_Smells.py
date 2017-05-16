6a396a118b4fcff7dee68ee4b08f38599395cc2e
Dan Decker <danieldecker88@gmail.com>
2017-05-16 13:04:40 -0400

    Create Karsten_Smells.py

---
+++ utils/Karsten_Smells.py
@@ -0,0 +1,17 @@
+import os
+import sys 
+
+def karsten_sucks(): 
+    while 1: 
+        print "Karsten sucks"
+        
+def karsten_smells(): 
+    smell_adj = ["stinks","smells","reeks"]
+    for x in smell_adj: 
+        print "Karsten "+x
+        
+# Main 
+karsten_smells()
+karsten_sucks()
+
+print 'Jk lololol 420 swag yolo!'
